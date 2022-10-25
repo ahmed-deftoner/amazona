06ba5f5efa568f44334a216ec19e9b6ef9e52f71
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-25 16:55:00 +0500

    mock test

---
+++ backend/tests/products.test.js
@@ -0,0 +1,9 @@
+describe("products", () => {
+    describe("get product routes", () => {
+        describe("given the product does not exist", () => {
+            it("expect 404", () => {
+                expect(true).toBe(true);
+            });
+        });
+    });
+});
