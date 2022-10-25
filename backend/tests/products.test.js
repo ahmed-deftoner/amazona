4d1b07cc37502c20c762bed4f03841fd982a0bed
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-25 19:13:48 +0500

    configured sucessfully

--- backend/tests/products.test.js
+++ backend/tests/products.test.js
@@ -1,11 +1,11 @@
-const {app} = require("../server")
-const request = require("superagent")
+import request from "supertest";
+import {app} from "../server";
 
 describe("products", () => {
     describe("get product routes", () => {
         describe("given the product does not exist", () => {
             it("expect 404",async () => {
-                const productId = "product-123"
+                const productId = "product-123";
                 await request(app).get(`/api/products/${productId}`).expect(404);
             });
         });
